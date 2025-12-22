20859fe5eca65f0bf46234477fa560db399fb893
Alex <oleksandr100204@gmail.com>
2025-12-22 18:04:41 +0100

    changed fonts for logo and added icons for skills block

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import './styles/main.scss'
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+
+      const spans = entry.target.querySelectorAll('li span ');
+
+      spans.forEach((span, index) => {
+        span.style.animationDelay = `${index * 150}ms`;
+        span.classList.add('is-visible');
+      });
+
+      observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.6
+  }
+);
+
+document.querySelectorAll('.animate-on-scroll').forEach(list => {
+  observer.observe(list);
+});
