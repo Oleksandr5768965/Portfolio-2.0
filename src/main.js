bd104e19e8670b8a1011c7187b0be95d98af245a
Alex <oleksandr100204@gmail.com>
2025-12-25 17:30:17 +0100

    make some changes and added small validation and animation

--- src/main.js
+++ src/main.js
@@ -4,22 +4,36 @@ const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach(entry => {
       if (!entry.isIntersecting) return;
-
       const spans = entry.target.querySelectorAll('li span ');
-
       spans.forEach((span, index) => {
         span.style.animationDelay = `${index * 150}ms`;
         span.classList.add('is-visible');
       });
-
       observer.unobserve(entry.target);
     });
   },
   {
-    threshold: 0.6
+    threshold: 0.6 //delay before start animation
   }
 );
 
-document.querySelectorAll('.animate-on-scroll').forEach(list => {
-  observer.observe(list);
+document.querySelectorAll('.animate-on-scroll').forEach(list => { 
+   observer.observe(list); 
+});
+
+document.querySelector("#send-form").addEventListener("submit", (event) => {
+   event.preventDefault();
+   const nameField = document.querySelector("#name");
+   const emailField = document.querySelector("#email");
+   const messageField = document.querySelector("#message");
+
+   if (nameField.value === "" || messageField.value === "" || emailField.value === "") { 
+    nameField.classList.toggle("form-error-state-animation");
+    messageField.classList.toggle("form-error-state-animation");
+    emailField.classList.toggle("form-error-state-animation");
+    console.log("Form error state");
+   } else {
+    // nameField.classList.remove("form-error-state-animation");
+    // messageField.classList.remove("form-error-state-animation");
+   }
 });
